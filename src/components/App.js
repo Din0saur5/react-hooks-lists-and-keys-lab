49c62f1bf8b59c31561b81900eb3541c31ce8c18
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-10-23 17:02:08 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
+      <Home user= {user}  />
       <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
     </div>
